c762ee523299601894b6381a5c007f7abe7431a1
XiaodongXie <xiexiaodong917@163.com>
2017-03-13 15:52:11 +0800

    :art:格式化xauth.go

--- xauth.go
+++ xauth.go
@@ -1,102 +1,102 @@
 package xauth
 
 import (
-    "sort"
-    "fmt"
-    "crypto/md5"
-    "encoding/hex"
-    "strings"
-    "time"
-    "log"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"sort"
+	"strings"
+	"time"
 )
 
 type Request struct {
-    data        XParamSlice
+	data XParamSlice
 }
 
 type XParam struct {
-    Key             string
-    Value           interface{}
+	Key   string
+	Value interface{}
 }
 
 type XParamSlice []*XParam
 
 func (x XParamSlice) Len() int {
-    return len(x)
+	return len(x)
 }
 
 func (x XParamSlice) Less(i, j int) bool {
-    return x[i].Key < x[j].Key
+	return x[i].Key < x[j].Key
 }
 
 func (x XParamSlice) Swap(i, j int) {
-    tmp := x[i]
-    x[i] = x[j]
-    x[j] = tmp
+	tmp := x[i]
+	x[i] = x[j]
+	x[j] = tmp
 }
 
 type XAuth struct {
-    AppKey          string                      `json:"appKey"`
-    Sign            string                      `json:"sign"`
-    Timestamp       int64                       `json:"timestamp"`
+	AppKey    string `json:"appKey"`
+	Sign      string `json:"sign"`
+	Timestamp int64  `json:"timestamp"`
 
-    AppSecret       string                      `json:"appSecret"`
-    Params          XParamSlice                 `json:"params"`
+	AppSecret string      `json:"appSecret"`
+	Params    XParamSlice `json:"params"`
 
-    KeyValues       string                      `json:"-"`
-    MD5Value        string                      `json:"-"`
+	KeyValues string `json:"-"`
+	MD5Value  string `json:"-"`
 }
 
 // 计算签名
 func (a *XAuth) GetSign() {
-    // 将参数key按照ASCII顺序排列
-    sort.Sort(a.Params)
+	// 将参数key按照ASCII顺序排列
+	sort.Sort(a.Params)
 
-    // 连接key和它对应的value
-    a.GetKeyValues()
+	// 连接key和它对应的value
+	a.GetKeyValues()
 
-    a.GetMD5()
+	a.GetMD5()
 
-    a.Sign = strings.ToUpper(a.MD5Value)
+	a.Sign = strings.ToUpper(a.MD5Value)
 }
 
 // 获取参数keyvalue值
 func (a *XAuth) GetKeyValues() {
-    keyValues := ""
-    for _, p := range a.Params {
-        keyValues += fmt.Sprintf("%s%v", p.Key, p.Value)
-    }
+	keyValues := ""
+	for _, p := range a.Params {
+		keyValues += fmt.Sprintf("%s%v", p.Key, p.Value)
+	}
 
-    a.KeyValues = keyValues
+	a.KeyValues = keyValues
 }
 
 // 计算MD5
 func (a *XAuth) GetMD5() {
-    hash := md5.New()
-    hash.Write([]byte(fmt.Sprintf("%s%s", a.AppSecret, a.KeyValues)))
-    src := hash.Sum(nil)
-    a.MD5Value = hex.EncodeToString(src)
+	hash := md5.New()
+	hash.Write([]byte(fmt.Sprintf("%s%s", a.AppSecret, a.KeyValues)))
+	src := hash.Sum(nil)
+	a.MD5Value = hex.EncodeToString(src)
 }
 
 // 判断timestamp是否过期，误差+-7分钟
 func (a *XAuth) IsExpired() bool {
-    currentTime := time.Now()
-    paramTime := time.Unix(a.Timestamp / 1000, 0)
+	currentTime := time.Now()
+	paramTime := time.Unix(a.Timestamp/1000, 0)
 
-    maxTime := paramTime.Add(time.Minute * 7)
-    minTime := paramTime.Add(-1 * time.Minute * 7)
+	maxTime := paramTime.Add(time.Minute * 7)
+	minTime := paramTime.Add(-1 * time.Minute * 7)
 
-    log.Println(maxTime, minTime, currentTime)
+	log.Println(maxTime, minTime, currentTime)
 
-    if currentTime.Before(maxTime) && currentTime.After(minTime) {
-        return false
-    }
+	if currentTime.Before(maxTime) && currentTime.After(minTime) {
+		return false
+	}
 
-    return true
+	return true
 }
 
 // 是否验证通过
 func (a *XAuth) IsAuthPass(targetSign string) bool {
-    a.GetSign()
-    return strings.EqualFold(a.Sign, targetSign)
+	a.GetSign()
+	return strings.EqualFold(a.Sign, targetSign)
 }
